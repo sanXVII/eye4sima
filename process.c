d2d5232d5c56bdc6d5cedab4c6bb21c29acbf2a7
Alexander <sanXVII@gmail.com>
2014-06-09 17:03:22 +0400

    Определение центра одноцветных пятен делаем

--- process.c
+++ process.c
@@ -35,17 +35,9 @@ typedef struct border_find_t
 static int mv_border( img_t * img, border_find_t * bf, int x, int y )
 {
 	uint8_t * c_pnt = img->buf + 3 * ( ( img->height - y ) * img->width + x );
-	int dr = /*( *c_pnt > *( bf->start_pnt ) ) ? 
-		( *c_pnt - *( bf->start_pnt ) ) :*/ ( *( bf->start_pnt ) - *c_pnt );
-
-	int dg = /*( *( c_pnt + 1 ) > *( bf->start_pnt + 1 ) ) ?
-		( *( c_pnt + 1 ) - *( bf->start_pnt + 1 ) ) : */
-		( *( bf->start_pnt + 1 ) - *( c_pnt + 1 ) );
-
-	int db = /*( *( c_pnt + 2 ) > *( bf->start_pnt + 2 ) ) ?
-		( *( c_pnt + 2 ) - *( bf->start_pnt + 2 ) ) : */
-		( *( bf->start_pnt + 2 ) - *( c_pnt + 2 ) );
-
+	int dr = ( *( bf->start_pnt ) - *c_pnt );
+	int dg = ( *( bf->start_pnt + 1 ) - *( c_pnt + 1 ) );
+	int db = ( *( bf->start_pnt + 2 ) - *( c_pnt + 2 ) );
 	int dv = dr * dr + dg * dg + db * db + bf->min_dv;
 
 	if( bf->cnt_dv )
@@ -66,7 +58,7 @@ static int mv_border( img_t * img, border_find_t * bf, int x, int y )
 
 #define RAY_LEN 100
 
-static void mk_horizont_border( img_t * img, int cx, int cy )
+static void one_X( img_t * img, int cx, int cy, float ang )
 {
 	border_find_t bf_data;
 	bf_data.start_pnt = img->buf + 3 * ( ( img->height - cy ) * img->width + cx );
@@ -75,41 +67,105 @@ static void mk_horizont_border( img_t * img, int cx, int cy )
 	bf_data.wall = 22; /* установлено шаманами */
 	bf_data.min_dv = 14; /* определено шаманами */
 
+	float st_x = cos( ang );
+	float st_y = sin( ang );
+
 	/* Нарисуем оси */
         glBegin( GL_LINES );
-        glColor3f(1, 1, 1);
-        glVertex3f( cx - RAY_LEN, cy, 0);
-        glVertex3f( cx + RAY_LEN, cy, 0);
-        glVertex3f( cx, cy - 2, 0);
-        glVertex3f( cx, cy + 2, 0);
+        glColor3f( 0.3, 0.3, 0.3 );
+        glVertex3f( cx - ( int )( RAY_LEN * st_x ), cy + ( int )( RAY_LEN * st_y ), 0 );
+        glVertex3f( cx + ( int )( RAY_LEN * st_x ), cy - ( int )( RAY_LEN * st_y ), 0 );
+        glVertex3f( cx - ( int )( RAY_LEN * st_y ), cy - ( int )( RAY_LEN * st_x ), 0 );
+        glVertex3f( cx + ( int )( RAY_LEN * st_y ), cy + ( int )( RAY_LEN * st_x ), 0 );
         glEnd();
 
 
-	int dx;
+	int i;
 
 	/* справа */
-	for( dx = 1; ( dx < 100 ) && ( cx + dx < img->width ); dx++ )
+	for( i = 1; i < RAY_LEN; i++ )
 	{
-		if( mv_border( img, &bf_data, cx + dx, cy ) )
+		int ix = cx + ( int )( i * st_x );
+		int iy = cy - ( int )( i * st_y );
+
+		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
+		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
+
+		if( mv_border( img, &bf_data, ix, iy ) )
 		{
 			glBegin( GL_LINES );
-			glColor3f( 0.8, 1, 0.8 );
-			glVertex3f( cx + dx, cy - 2, 0);
-			glVertex3f( cx + dx, cy + 2, 0);
+			glColor3f( 0.4, 1, 0.4 );
+			glVertex3f( ix - 2, iy - 2, 0);
+			glVertex3f( ix + 2, iy + 2, 0);
+			glVertex3f( ix - 2, iy + 2, 0);
+			glVertex3f( ix + 2, iy - 2, 0);
 			glEnd();
 			break;
 		}
 	}
 
 	/* слева */
-	for( dx = 1; ( dx < 100 ) && ( cx - dx >= 0 ); dx++ )
+	for( i = 1; i < RAY_LEN; i++ )
 	{
-		if( mv_border( img, &bf_data, cx - dx, cy ) )
+		int ix = cx - ( int )( i * st_x );
+		int iy = cy + ( int )( i * st_y );
+
+		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
+		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
+
+		if( mv_border( img, &bf_data, ix, iy ) )
 		{
 			glBegin( GL_LINES );
-			glColor3f( 0.8, 1, 0.8 );
-			glVertex3f( cx - dx, cy - 2, 0);
-			glVertex3f( cx - dx, cy + 2, 0);
+			glColor3f( 1, 1, 1 );
+			glVertex3f( ix - 2, iy - 2, 0);
+			glVertex3f( ix + 2, iy + 2, 0);
+			glVertex3f( ix - 2, iy + 2, 0);
+			glVertex3f( ix + 2, iy - 2, 0);
+			glEnd();
+			break;
+		}
+	}
+
+	/* сверху */
+	for( i = 1; i < RAY_LEN; i++ )
+	{
+		int ix = cx - ( int )( i * st_y );
+		int iy = cy - ( int )( i * st_x );
+
+		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
+		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
+
+		if( mv_border( img, &bf_data, ix, iy ) )
+		{
+			glBegin( GL_LINES );
+			glColor3f( 1, 1, 1 );
+			glVertex3f( ix - 2, iy - 2, 0);
+			glVertex3f( ix + 2, iy + 2, 0);
+			glVertex3f( ix - 2, iy + 2, 0);
+			glVertex3f( ix + 2, iy - 2, 0);
+			glEnd();
+			break;
+		}
+	}
+
+
+	/* снизу */
+	for( i = 1; i < RAY_LEN; i++ )
+	{
+		int ix = cx + ( int )( i * st_y );
+		int iy = cy + ( int )( i * st_x );
+
+		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
+		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
+
+		if( mv_border( img, &bf_data, ix, iy ) )
+		{
+			glBegin( GL_LINES );
+			glColor3f( 1, 1, 1 );
+			glVertex3f( ix - 2, iy - 2, 0);
+			glVertex3f( ix + 2, iy + 2, 0);
+			glVertex3f( ix - 2, iy + 2, 0);
+			glVertex3f( ix + 2, iy - 2, 0);
 			glEnd();
 			break;
 		}
@@ -123,14 +179,11 @@ void process_rgb_frame( uint8_t *img, int img_width, int img_height )
 	frame.width = img_width;
 	frame.height = img_height;
 
-	
 
-	/* Тут мы будем искать все наши маркеры.
-	 * Также можно отрисовать через opengl */
-	int a;
-	for( a = 2; a < 80; a++ )
-	{
-		mk_horizont_border( &frame, img_width / 2, 10 + a * 5 );
+	int i;
+	for( i = 0; i < 100; i++ )
+	{	
+		one_X( &frame, img_width / 2, img_height / 2, (float)( rand() % 1000 ) * 2 * M_PI / 1000.0 );
 	}
 }
 
