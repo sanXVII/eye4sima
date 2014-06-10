a1118ad4ee697a558b473c22c3e9d00d5e5e8c8f
Alexander <sanXVII@gmail.com>
2014-06-10 17:20:09 +0400

    ++

--- process.c
+++ process.c
@@ -27,13 +27,18 @@ typedef struct border_find_t
 	int cnt_dv;
 	int min_dv;
 	int wall;
-	
+
+	int result;
+
 } border_find_t;
 
 
 
-static int mv_border( img_t * img, border_find_t * bf, int x, int y )
+static int mv_border( img_t * img, border_find_t * bf, int x, int y, int way )
 {
+	if( ( x >= img->width ) || ( x < 0 ) ) return -1;
+	if( ( y >= img->height ) || ( y < 0 ) ) return -1;
+
 	uint8_t * c_pnt = img->buf + 3 * ( ( img->height - y ) * img->width + x );
 	int dr = ( *( bf->start_pnt ) - *c_pnt );
 	int dg = ( *( bf->start_pnt + 1 ) - *( c_pnt + 1 ) );
@@ -45,6 +50,16 @@ static int mv_border( img_t * img, border_find_t * bf, int x, int y )
 		/* а не граница ли тут */
 		if( dv * bf->cnt_dv > bf->sum_dv * bf->wall )
 		{
+			glBegin( GL_LINES );
+			if( way == 0x01 ) glColor3f( 0.4, 1, 0.4 ); else glColor3f( 1, 0.4, 0.4 );
+			glVertex3f( x - 2, y - 2, 0);
+			glVertex3f( x + 2, y + 2, 0);
+			glVertex3f( x - 2, y + 2, 0);
+			glVertex3f( x + 2, y - 2, 0);
+			glEnd();
+
+			bf->result |= way;
+
 			return 1; /* есть граница */
 		}
 	}
@@ -58,14 +73,18 @@ static int mv_border( img_t * img, border_find_t * bf, int x, int y )
 
 #define RAY_LEN 100
 
-static void one_X( img_t * img, int cx, int cy, float ang )
+static int one_X( img_t * img, int * px, int * py, float ang )
 {
+	int cx = *px;
+	int cy = *py;
+
 	border_find_t bf_data;
 	bf_data.start_pnt = img->buf + 3 * ( ( img->height - cy ) * img->width + cx );
 	bf_data.sum_dv = 0;
 	bf_data.cnt_dv = 0;
 	bf_data.wall = 22; /* установлено шаманами */
 	bf_data.min_dv = 14; /* определено шаманами */
+	bf_data.result = 0;
 
 	float st_x = cos( ang );
 	float st_y = sin( ang );
@@ -81,95 +100,63 @@ static void one_X( img_t * img, int cx, int cy, float ang )
 
 
 	int i;
+	int ix;
+	int iy;
+	int ret_x = 0;
+	int ret_y = 0;
 
 	/* справа */
 	for( i = 1; i < RAY_LEN; i++ )
 	{
-		int ix = cx + ( int )( i * st_x );
-		int iy = cy - ( int )( i * st_y );
+		ix = cx + ( int )( i * st_x );
+		iy = cy - ( int )( i * st_y );
 
-		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
-		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
-
-		if( mv_border( img, &bf_data, ix, iy ) )
-		{
-			glBegin( GL_LINES );
-			glColor3f( 0.4, 1, 0.4 );
-			glVertex3f( ix - 2, iy - 2, 0);
-			glVertex3f( ix + 2, iy + 2, 0);
-			glVertex3f( ix - 2, iy + 2, 0);
-			glVertex3f( ix + 2, iy - 2, 0);
-			glEnd();
-			break;
-		}
+		if( mv_border( img, &bf_data, ix, iy, 0x01/* право */ ) ) break;
 	}
+	ret_x += ix;
+	ret_y += iy;
 
 	/* слева */
 	for( i = 1; i < RAY_LEN; i++ )
 	{
-		int ix = cx - ( int )( i * st_x );
-		int iy = cy + ( int )( i * st_y );
+		ix = cx - ( int )( i * st_x );
+		iy = cy + ( int )( i * st_y );
 
-		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
-		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
-
-		if( mv_border( img, &bf_data, ix, iy ) )
-		{
-			glBegin( GL_LINES );
-			glColor3f( 1, 1, 1 );
-			glVertex3f( ix - 2, iy - 2, 0);
-			glVertex3f( ix + 2, iy + 2, 0);
-			glVertex3f( ix - 2, iy + 2, 0);
-			glVertex3f( ix + 2, iy - 2, 0);
-			glEnd();
-			break;
-		}
+		if( mv_border( img, &bf_data, ix, iy, 0x02/* лево */ ) ) break;
 	}
+	ret_x += ix;
+	ret_y += iy;
 
 	/* сверху */
 	for( i = 1; i < RAY_LEN; i++ )
 	{
-		int ix = cx - ( int )( i * st_y );
-		int iy = cy - ( int )( i * st_x );
+		ix = cx - ( int )( i * st_y );
+		iy = cy - ( int )( i * st_x );
 
-		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
-		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
-
-		if( mv_border( img, &bf_data, ix, iy ) )
-		{
-			glBegin( GL_LINES );
-			glColor3f( 1, 1, 1 );
-			glVertex3f( ix - 2, iy - 2, 0);
-			glVertex3f( ix + 2, iy + 2, 0);
-			glVertex3f( ix - 2, iy + 2, 0);
-			glVertex3f( ix + 2, iy - 2, 0);
-			glEnd();
-			break;
-		}
+		if( mv_border( img, &bf_data, ix, iy, 0x04/* вверх */ ) ) break;
 	}
+	ret_x += ix;
+	ret_y += iy;
 
 
 	/* снизу */
 	for( i = 1; i < RAY_LEN; i++ )
 	{
-		int ix = cx + ( int )( i * st_y );
-		int iy = cy + ( int )( i * st_x );
+		ix = cx + ( int )( i * st_y );
+		iy = cy + ( int )( i * st_x );
 
-		if( ( ix >= img->width ) || ( ix < 0 ) ) break;
-		if( ( iy >= img->height ) || ( iy < 0 ) ) break;
-
-		if( mv_border( img, &bf_data, ix, iy ) )
-		{
-			glBegin( GL_LINES );
-			glColor3f( 1, 1, 1 );
-			glVertex3f( ix - 2, iy - 2, 0);
-			glVertex3f( ix + 2, iy + 2, 0);
-			glVertex3f( ix - 2, iy + 2, 0);
-			glVertex3f( ix + 2, iy - 2, 0);
-			glEnd();
-			break;
-		}
+		if( mv_border( img, &bf_data, ix, iy, 0x08/* вниз */ ) ) break;
 	}
+	ret_x += ix;
+	ret_y += iy;
+
+	if( bf_data.result == 0x0f/* все границы */ )
+	{
+		*px = ret_x / 4;
+		*py = ret_y / 4;
+		return 1; 
+	}
+	return 0;
 }
 
 void process_rgb_frame( uint8_t *img, int img_width, int img_height )
@@ -181,9 +168,11 @@ void process_rgb_frame( uint8_t *img, int img_width, int img_height )
 
 
 	int i;
+	int x = img_width / 2;
+	int y = img_height / 2;
 	for( i = 0; i < 100; i++ )
-	{	
-		one_X( &frame, img_width / 2, img_height / 2, (float)( rand() % 1000 ) * 2 * M_PI / 1000.0 );
+	{
+		one_X( &frame, &x, &y, (float)( rand() % 1000 ) * 2 * M_PI / 1000.0 );
 	}
 }
 
