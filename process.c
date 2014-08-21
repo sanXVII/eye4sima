2dd9c4030046b9977b3e09449c8481b6174f4dcf
Alexander <sanXVII@gmail.com>
2014-08-21 14:05:11 +0400

    Added the new detector of centers.

--- process.c
+++ process.c
@@ -28,51 +28,19 @@ typedef struct border_find_t
 	int min_dv;
 	int wall;
 
-	int result;
+	int start_x;
+	int start_y;
+
+	int x;
+	int y;
+
+	int new_center_x;
+	int new_center_y;
 
 } border_find_t;
 
-
-
-static int mv_border( img_t * img, border_find_t * bf, int x, int y, int way )
-{
-	if( ( x >= img->width ) || ( x < 0 ) ) return 0;
-	if( ( y >= img->height ) || ( y < 0 ) ) return 0;
-
-	uint8_t * c_pnt = img->buf + 3 * ( ( img->height - y ) * img->width + x );
-	int dr = ( *( bf->start_pnt ) - *c_pnt );
-	int dg = ( *( bf->start_pnt + 1 ) - *( c_pnt + 1 ) );
-	int db = ( *( bf->start_pnt + 2 ) - *( c_pnt + 2 ) );
-	int dv = dr * dr + dg * dg + db * db + bf->min_dv;
-
-	if( bf->cnt_dv )
-	{
-		/* а не граница ли тут */
-		if( dv * bf->cnt_dv > bf->sum_dv * bf->wall )
-		{
-			glBegin( GL_LINES );
-			if( way == 0x01 ) glColor3f( 0.4, 1, 0.4 ); else glColor3f( 1, 0.4, 0.4 );
-			glVertex3f( x - 2, y - 2, 0);
-			glVertex3f( x + 2, y + 2, 0);
-			glVertex3f( x - 2, y + 2, 0);
-			glVertex3f( x + 2, y - 2, 0);
-			glEnd();
-
-			bf->result |= way;
-
-			return 1; /* есть граница */
-		}
-	}
-
-	bf->cnt_dv++;
-	bf->sum_dv += dv;
-
-	return 0;
-}
-
-
 #define RAY_LEN 100
-#define X_CYCLE_CNT 90
+#define X_CYCLE_CNT 100
 typedef struct maybe_figure
 {
 	int enter_x;
@@ -90,19 +58,75 @@ typedef struct maybe_figure
 } maybe_figure;
 
 
+static int mv_border( img_t * img, border_find_t * bf, maybe_figure * fig, float st_x, float st_y )
+{
+	int i;
+
+	for( i = 1; i < RAY_LEN; i++ )
+	{
+		bf->x = bf->start_x + ( int )( i * st_x ); //------------------
+		bf->y = bf->start_y - ( int )( i * st_y ); //------------------
+
+
+		if( ( bf->x >= img->width ) || ( bf->x < 0 ) ) return 0;
+		if( ( bf->y >= img->height ) || ( bf->y < 0 ) ) return 0;
+
+		uint8_t * c_pnt = img->buf + 3 * ( ( img->height - bf->y ) * img->width + bf->x );
+		int dr = ( *( bf->start_pnt ) - *c_pnt );
+		int dg = ( *( bf->start_pnt + 1 ) - *( c_pnt + 1 ) );
+		int db = ( *( bf->start_pnt + 2 ) - *( c_pnt + 2 ) );
+		int dv = dr * dr + dg * dg + db * db + bf->min_dv;
+
+		if( bf->cnt_dv )
+		{
+			/* а не граница ли тут */
+			if( dv * bf->cnt_dv > bf->sum_dv * bf->wall )
+			{
+				bf->new_center_x += bf->x;
+				bf->new_center_y += bf->y;
+
+				fig->point_Xs[ fig->point_cnt ] = bf->x;
+				fig->point_Ys[ fig->point_cnt ] = bf->y;
+				fig->point_cnt++;
+
+				glBegin( GL_LINES );
+				glColor3f( 1, 0.4, 0.4 );
+				glVertex3f( bf->x - 2, bf->y - 2, 0);
+				glVertex3f( bf->x + 2, bf->y + 2, 0);
+				glVertex3f( bf->x - 2, bf->y + 2, 0);
+				glVertex3f( bf->x + 2, bf->y - 2, 0);
+				glEnd();
+
+				return 1; /* есть граница */
+			}
+		}
+
+		bf->cnt_dv++;
+		bf->sum_dv += dv;
+	}
+
+	return 0; /* нет границы */
+}
+
+
+
+
 
 static int one_X( img_t * img, maybe_figure * fig, float ang )
 {
-	int cx = fig->enter_x;
-	int cy = fig->enter_y;
 
 	border_find_t bf_data;
-	bf_data.start_pnt = img->buf + 3 * ( ( img->height - cy ) * img->width + cx );
+	bf_data.start_pnt = img->buf + 3 * 
+		( ( img->height - fig->enter_y ) * img->width + fig->enter_x );
+
 	bf_data.sum_dv = 0;
 	bf_data.cnt_dv = 0;
 	bf_data.wall = 22; /* установлено шаманами */
 	bf_data.min_dv = 14; /* определено шаманами */
-	bf_data.result = 0;
+	bf_data.start_x = fig->enter_x;
+	bf_data.start_y = fig->enter_y;
+	bf_data.new_center_x = 0;
+	bf_data.new_center_y = 0;
 
 	float st_x = cos( ang );
 	float st_y = sin( ang );
@@ -116,108 +140,52 @@ static int one_X( img_t * img, maybe_figure * fig, float ang )
         //glVertex3f( cx + ( int )( RAY_LEN * st_y ), cy + ( int )( RAY_LEN * st_x ), 0 );
         //glEnd();
 
-
-	int i;
-	int ix;
-	int iy;
-
-	/* Центр одного последнего крестика */
-	int ret_x = 0;
-	int ret_y = 0;
-
 	/* справа */
-	for( i = 1; i < RAY_LEN; i++ )
-	{
-		ix = cx + ( int )( i * st_x );
-		iy = cy - ( int )( i * st_y );
-
-		if( mv_border( img, &bf_data, ix, iy, 0x01/* право */ ) ) break;
-	}
-	if( i == RAY_LEN ) return 0; /* Abort */
-
-	ret_x += ix;
-	ret_y += iy;
-
-	fig->point_Xs[ fig->point_cnt ] = ix;
-	fig->point_Ys[ fig->point_cnt ] = iy;
-	fig->point_cnt++;
+	if( !mv_border( img, &bf_data, fig, st_x, st_y * (-1) ) )
+		return 0; /* Abort */
 
 	/* слева */
-	for( i = 1; i < RAY_LEN; i++ )
-	{
-		ix = cx - ( int )( i * st_x );
-		iy = cy + ( int )( i * st_y );
-
-		if( mv_border( img, &bf_data, ix, iy, 0x02/* лево */ ) ) break;
-	}
-	if( i == RAY_LEN ) return 0; /* Abort */
-
-	ret_x += ix;
-	ret_y += iy;
-
-	fig->point_Xs[ fig->point_cnt ] = ix;
-	fig->point_Ys[ fig->point_cnt ] = iy;
-	fig->point_cnt++;
+	if( !mv_border( img, &bf_data, fig, st_x * (-1), st_y ) )
+		return 0;
 
 	/* сверху */
-	for( i = 1; i < RAY_LEN; i++ )
-	{
-		ix = cx - ( int )( i * st_y );
-		iy = cy - ( int )( i * st_x );
-
-		if( mv_border( img, &bf_data, ix, iy, 0x04/* вверх */ ) ) break;
-	}
-	if( i == RAY_LEN ) return 0; /* Abort */
-
-	ret_x += ix;
-	ret_y += iy;
-
-	fig->point_Xs[ fig->point_cnt ] = ix;
-	fig->point_Ys[ fig->point_cnt ] = iy;
-	fig->point_cnt++;
+	if( !mv_border( img, &bf_data, fig, st_x * (-1), st_y * (-1) ) )
+		return 0;
 
 	/* снизу */
-	for( i = 1; i < RAY_LEN; i++ )
-	{
-		ix = cx + ( int )( i * st_y );
-		iy = cy + ( int )( i * st_x );
+	if( !mv_border( img, &bf_data, fig, st_x, st_y ) )
+		return 0;
 
-		if( mv_border( img, &bf_data, ix, iy, 0x08/* вниз */ ) ) break;
-	}
-	if( i == RAY_LEN ) return 0; /* Abort */
-
-	ret_x += ix;
-	ret_y += iy;
-
-	fig->point_Xs[ fig->point_cnt ] = ix;
-	fig->point_Ys[ fig->point_cnt ] = iy;
-	fig->point_cnt++;
-
-	if( bf_data.result == 0x0f/* все границы */ )
-	{
-		fig->enter_x = ret_x / 4;
-		fig->enter_y = ret_y / 4;
-		return 1; 
-	}
-	return 0;
+	fig->enter_x = bf_data.new_center_x / 4;
+	fig->enter_y = bf_data.new_center_y / 4;
+	return 1; 
 }
 
 
 
 int X_cycle( img_t * frame, maybe_figure * fig )
 {
+	int fail_cnt = 0;
+
 	/* Сбросим точки края старой фигурки .. будем искать новую */
 	fig->point_cnt = 0;
 
 	int ii;
 	for( ii = 0; ii < X_CYCLE_CNT; ii++ )
 	{
-		if( !one_X( frame, fig, (float)( rand() % 1000 ) * 2 * M_PI / 1000.0 ) ) 
-			break;
+		if( !one_X( frame, fig, (float)( rand() % 1000 ) * 2 * M_PI / 1000.0 ) )
+		{
+			if( ( fail_cnt++ ) > ( X_CYCLE_CNT / 50 ) )
+			{
+				/* Слишком много ошибок подряд */
+				return 0; /* A figure is not detected. */
+			}
+		}
+		else
+		{
+			fail_cnt = 0;
+		}
 	}
-
-	if( ii < ( X_CYCLE_CNT / 3 ) )
-		return 0; /* A figure is not detected. */
 
 	/* Найти центр */
 	int i;
